0e075692f451482c178a3b40a94a05b954b45068
Christopher Wolters <christopherwolters@MBPvonCistopher.wired.duke.local>
2022-06-07 17:32:53 -0400

    Attempt to resovle dimensionality issue

--- Training_pytorch/cifar/model.py
+++ Training_pytorch/cifar/model.py
@@ -10,7 +10,8 @@ class CIFAR(nn.Module):
         assert isinstance(features, nn.Sequential), type(features)
         self.features = features
         self.classifier = nn.Sequential(
-            QLinear(8192, 1024, logger=logger,
+            # 8192 for Cifar, 4608 for mnist
+            QLinear(4608, 1024, logger=logger,
                     wl_input = args.wl_activate,wl_activate=args.wl_activate,wl_error=args.wl_error,
                     wl_weight=args.wl_weight,inference=args.inference,onoffratio=args.onoffratio,cellBit=args.cellBit,
                     subArray=args.subArray,ADCprecision=args.ADCprecision,vari=args.vari,t=args.t,v=args.v,detect=args.detect,target=args.target, name='FC1_'),
