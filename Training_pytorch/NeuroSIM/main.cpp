5450da905a70debe7f18f7fefe23dbb8b75911dc
chWol <ch.wolters@tum.de>
2022-06-10 15:59:34 -0400

    Starting wiring again

--- Training_pytorch/NeuroSIM/main.cpp
+++ Training_pytorch/NeuroSIM/main.cpp
@@ -74,8 +74,6 @@ int main(int argc, char * argv[]) {
 	param->numBitInput = atoi(argv[4]);            		 // precision of input neural activation
 	param->batchSize = atoi(argv[5]);
 
-	cout << param->batchSize;
-
 	if (param->cellBit > param->synapseBit) {
 		cout << "ERROR!: Memory precision is even higher than synapse precision, please modify 'cellBit' in Param.cpp!" << endl;
 		param->cellBit = param->synapseBit;
