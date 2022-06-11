32c93ac66083b65cfbd5a48659264f08f85379cd
chWol <ch.wolters@tum.de>
2022-06-10 21:16:07 -0400

    Remove test of cyclical dependency

--- Training_pytorch/NeuroSIM/Param.cpp
+++ Training_pytorch/NeuroSIM/Param.cpp
@@ -56,7 +56,7 @@ Param::Param() {
 	/***************************************** user defined design options and parameters *****************************************/
 	operationmode = 2;     		// 1: conventionalSequential (Use several multi-bit RRAM as one synapse)
 								// 2: conventionalParallel (Use several multi-bit RRAM as one synapse)
-    cout << param->memcelltype;
+
 	memcelltype = 3;        	// 1: cell.memCellType = Type::SRAM
 								// 2: cell.memCellType = Type::RRAM
 								// 3: cell.memCellType = Type::FeFET
