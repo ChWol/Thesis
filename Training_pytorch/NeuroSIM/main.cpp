3b23b463ef04bc76c90dade279c521b0fc11f448
chWol <ch.wolters@tum.de>
2022-06-14 11:18:19 -0400

    Add layer estimation to WandB

--- Training_pytorch/NeuroSIM/main.cpp
+++ Training_pytorch/NeuroSIM/main.cpp
@@ -359,7 +359,7 @@ int main(int argc, char * argv[]) {
 		// show the detailed hardware performance for each layer
 		for (int i=0; i<netStructure.size(); i++) {
 			cout << "-------------------- Estimation of Layer " << i+1 << " ----------------------" << endl;
-
+            cout << "TESTSTETETETETET HIER";
 			param->activityRowReadWG = atof(argv[4*i+16]);
                         param->activityRowWriteWG = atof(argv[4*i+16]);
                         param->activityColWriteWG = atof(argv[4*i+16]);
