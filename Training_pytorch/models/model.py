b663f9de4e8e90c8816dda2b00add3333f7281c5
chWol <ch.wolters@tum.de>
2022-06-17 11:59:13 -0400

    Changing layer structure

--- Training_pytorch/models/model.py
+++ Training_pytorch/models/model.py
@@ -104,7 +104,7 @@ def make_classifiers(classifiers, args, logger, in_dimension):
                          wl_input=args.wl_activate, wl_activate=wl_activate, wl_error=args.wl_error,
                          wl_weight=args.wl_weight, inference=args.inference, onoffratio=args.onoffratio,
                          cellBit=args.cellBit, subArray=args.subArray, ADCprecision=args.ADCprecision, vari=args.vari,
-                         t=args.t, v=args.v, detect=args.detect, target=args.target, name='FC' + str(i) + '_'),
+                         t=args.t, v=args.v, detect=args.detect, target=args.target, name='FC' + str(i) + '_')
 
         if i == len(classifiers) - 1:
             layers += [linear]
