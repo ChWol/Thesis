048952551a65aa33feec08d5ffd78c7d5d6d0f04
chWol <ch.wolters@tum.de>
2022-06-24 13:44:11 -0400

    Removed print statements

--- Training_pytorch/modules/quantization_cpu_np_infer.py
+++ Training_pytorch/modules/quantization_cpu_np_infer.py
@@ -47,7 +47,6 @@ class QConv2d(nn.Conv2d):
         bitActivation = int(self.wl_input)
 
         if self.inference == 1:
-            print("INFERENCE in Conv")
             # retention
             weight = wage_quantizer.Retention(weight, self.t, self.v, self.detect, self.target)
             # set parameters for Hardware Inference
@@ -237,7 +236,6 @@ class QLinear(nn.Linear):
         bitActivation = int(self.wl_input)
 
         if self.inference == 1:
-            print("INFERENCE in Lin")
             # retention
             weight = wage_quantizer.Retention(weight, self.t, self.v, self.detect, self.target)
             # set parameters for Hardware Inference
