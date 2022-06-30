f8ebffcad9e363355f20a1f6b61d3c779a136892
chWol <ch.wolters@tum.de>
2022-06-30 17:04:20 -0400

    Fixed loss for testing

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -198,7 +198,6 @@ try:
             loss = wage_util.SSE(output, target)
 
             if args.rule == 'dfa':
-                print(model.parameters().size())
                 print(model.parameters())
 
                 gradients = [*transposed(loss), loss]
