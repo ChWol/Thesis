1c2aa7ef749cbf998d31c97d20b34f00a7c5e7bf
chWol <ch.wolters@tum.de>
2022-06-30 16:33:44 -0400

    Removed obsolete printe statement

--- Training_pytorch/utee/wage_util.py
+++ Training_pytorch/utee/wage_util.py
@@ -4,6 +4,5 @@ import torch
 def SSE(logits, label):
     target = torch.zeros_like(logits)
     target[torch.arange(target.size(0)).long(), label] = 1
-    print("Single errors")
     out = 0.5 * ((logits - target) ** 2)
     return out
