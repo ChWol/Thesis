a512c4a364a0944ebc0c7cc7a748f78277a6b0aa
chWol <ch.wolters@tum.de>
2022-06-30 16:24:53 -0400

    Dimensionality debugging

--- Training_pytorch/utee/wage_util.py
+++ Training_pytorch/utee/wage_util.py
@@ -4,5 +4,7 @@ import torch
 def SSE(logits, label):
     target = torch.zeros_like(logits)
     target[torch.arange(target.size(0)).long(), label] = 1
+    print("Single errors")
+    print(0.5 * ((logits - target) ** 2))
     out = 0.5 * ((logits - target) ** 2).sum()
     return out
