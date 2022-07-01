47a0ad3212d2cb80b7e8bed526af62aab7019bb0
chWol <ch.wolters@tum.de>
2022-07-01 10:13:57 -0400

    Dimensionality issues

--- Training_pytorch/utee/wage_util.py
+++ Training_pytorch/utee/wage_util.py
@@ -5,5 +5,8 @@ def SSE(logits, label):
     target = torch.zeros_like(logits)
     target[torch.arange(target.size(0)).long(), label] = 1
     out = 0.5 * ((logits - target) ** 2)
+    test = out.copy()
+    map(sum, zip(*test))
+    print(test)
     print("SSE output: {}".format(len(out[0])))
     return out
