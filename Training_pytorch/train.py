636c44086c30fd5611c55fcc04859b0935cdfaa7
chWol <ch.wolters@tum.de>
2022-07-06 10:28:00 -0400

    change position of optimizer

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -144,7 +144,7 @@ if args.cuda:
 
 #Todo: Add momentum and weight decay
 # torch.optim.SGD(model_fa.parameters(), lr=1e-4, momentum=0.9, weight_decay=0.001, nesterov=True)
-
+optimizer = optim.SGD(model.parameters(), lr=1)
 
 decreasing_lr = list(map(int, args.decreasing_lr.split(',')))
 logger('decreasing_lr: ' + str(decreasing_lr))
@@ -189,7 +189,7 @@ try:
             if args.cuda:
                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data), Variable(target)
-            #optimizer.zero_grad()
+            optimizer.zero_grad()
             output = model(data)
             error = wage_util.SSE(output, target)
             loss = 0.5 * (error ** 2)
@@ -213,7 +213,6 @@ try:
                 j = j + 1
 
             # Update function
-            optimizer = optim.SGD(model.parameters(), lr=1)
             optimizer.step()
 
             for name, param in list(model.named_parameters())[::-1]:
