2a82b674feb2da584685d5960598164ab228f8a8
chWol <ch.wolters@tum.de>
2022-07-08 16:38:45 -0400

    Neurosim trouble shooting

--- Training_pytorch/utee/hook.py
+++ Training_pytorch/utee/hook.py
@@ -126,8 +126,6 @@ def hardware_evaluation(model, wl_weight, wl_activation, numEpoch, batchSize, ce
     if os.path.exists('./layer_record/trace_command.sh'):
         os.remove('./layer_record/trace_command.sh')
     f = open('./layer_record/trace_command.sh', "w")
-    # Todo: From V1.3
-    # ./NeuroSIM/NetWork_'+str(model_name)+'.csv
     f.write('./NeuroSIM/main ' + str(numEpoch) + ' ./NeuroSIM/NetWork.csv ' + str(wl_weight) + ' ' + str(wl_activation)
             + ' ' + str(batchSize) + ' ' + str(cellBit) + ' ' + str(technode) + ' ' + str(wireWidth) + ' '
             + str(relu) + ' ' + str(memcelltype) + ' ' + str(levelOutput) + ' ' + str(onoffratio) + ' ')
@@ -137,6 +135,7 @@ def hardware_evaluation(model, wl_weight, wl_activation, numEpoch, batchSize, ce
     for i, layer in enumerate(model.classifier.modules()):
         if isinstance(layer, QLinear):
             hook_handle_list.append(layer.register_forward_hook(Neural_Sim))
+    print(hook_handle_list)
     return hook_handle_list
 
 
