0a4933619886f935c331c16201fe6e83063bfade
chWol <ch.wolters@tum.de>
2022-07-12 17:50:52 -0400

    testing

--- Training_pytorch/models/models.py
+++ Training_pytorch/models/models.py
@@ -261,7 +261,7 @@ def mnist(args, logger, num_classes, pretrained=None):
     if len(features) == 0:
         input = 784
     else:
-        input = 4608
+        input = 25088
 
     build_csv(features, classifiers, input, 1)
 
