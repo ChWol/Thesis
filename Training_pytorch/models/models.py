77594e1f8bec45e795ad8c8e3fd4497333be9a92
chWol <ch.wolters@tum.de>
2022-07-12 17:50:08 -0400

    testing

--- Training_pytorch/models/models.py
+++ Training_pytorch/models/models.py
@@ -156,7 +156,7 @@ def get_model(num_classes, network):
             'classifier': [('L', num_classes, 1, 'same', 1)]
         },
         'two': {
-            'features': [('C', 128, 1, 'same', 1)],
+            'features': [('C', 128, 1, 'same', 2), ('M', 2, 2)],
             'classifier': [('L', 800, 1, 'same', 1),
                            ('L', num_classes, 1, 'same', 1)]
         },
