749bbd56e6c7c06f522cdb345a978714bef79f92
chWol <ch.wolters@tum.de>
2022-07-23 13:30:52 -0400

    Messing with lr

--- Training_pytorch/models/models.py
+++ Training_pytorch/models/models.py
@@ -60,7 +60,7 @@ class MODEL(nn.Module):
                 layer.weight.grad = torch.matmul(torch.matmul(B, e) * a, y)
             if i == 0:
                 print("0: {}".format(torch.norm(layer.weight)))
-            if i == len(self.classifier):
+            if i == len(self.classifier)-1:
                 print("3: {}".format(torch.norm(layer.weight)))
 
 def build_csv(features, classifiers, linear_dimension, input_depth=3):
