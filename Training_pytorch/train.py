6ac0a2c2cfa0b81f9cc9158f95907bdbfa205d51
chWol <ch.wolters@tum.de>
2022-07-23 13:27:06 -0400

    Messing with lr

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -113,9 +113,10 @@ if args.cuda:
 
 if args.optimizer == 'adam':
     # optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-    optimizer = optim.Adam([{"params": model.classifier[0].parameters(), "lr": 1e-1},
+    optimizer = optim.Adam([{"params": model.classifier[0].parameters(), "lr": 1e-2},
                             {"params": model.classifier[1].parameters(), "lr": 1e-3},
-                            {"params": model.classifier[2].parameters(), "lr": 1e-3}],
+                            {"params": model.classifier[2].parameters(), "lr": 1e-3},
+                            {"params": model.classifier[3].parameters(), "lr": 1e-3}],
                            lr=args.learning_rate)
 else:
     optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9)
