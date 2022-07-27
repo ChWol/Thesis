626a4a18ee229e29b0e78a9b6d90d3a013c46d7a
chWol <ch.wolters@tum.de>
2022-07-27 12:47:25 -0400

    Test on network narrowness

--- Training_pytorch/NeuroSIM/Chip.cpp
+++ Training_pytorch/NeuroSIM/Chip.cpp
@@ -568,7 +568,7 @@ vector<double> ChipCalculateArea(InputParameter& inputParameter, Technology& tec
 
 	// ToDo: Here the desiredNumTileCM is important to be updated according to the additional matrix?
 	// Maybe we only need an additional Processing Element or only an additional SubArray?
-	area += CMTileArea*(desiredNumTileCM+1);
+	area += CMTileArea*(desiredNumTileCM);
 	areaIC += CMTileAreaIC*desiredNumTileCM;
 	areaADC += CMTileAreaADC*desiredNumTileCM;
 	areaAccum += CMTileAreaAccum*desiredNumTileCM;
