631b98a6f106c17f31c62dfd2844cac255fdc326
chWol <ch.wolters@tum.de>
2022-08-05 21:42:16 -0400

    additional hardware

--- Training_pytorch/NeuroSIM/main.cpp
+++ Training_pytorch/NeuroSIM/main.cpp
@@ -249,7 +249,7 @@ int main(int argc, char * argv[]) {
     double numComputation_BP = 0;
 	if (param->trainingEstimation) {
 		numComputation_BP = 2 * numComputation_Forward;
-		// numComputation_BP -= 2*(netStructure[0][0] * netStructure[0][1] * netStructure[0][2] * netStructure[0][3] * netStructure[0][4] * netStructure[0][5]);
+		numComputation_BP -= 2*(netStructure[0][0] * netStructure[0][1] * netStructure[0][2] * netStructure[0][3] * netStructure[0][4] * netStructure[0][5]);
 	}
     double  numComputation_DFA = 0;
 	if (param->trainingEstimation) {
@@ -289,7 +289,7 @@ int main(int argc, char * argv[]) {
 
 	    // FLOPs approach
 	    scalingFactor_WG = flopsDFA / flopsBP;
-	    scalingFactor_Total = (scalingFactor_WG * (numComputation_BP - numComputation_Forward) + 2 * numComputation_Forward) / (numComputation_Forward + numComputation_BP);
+	    scalingFactor_Total = (scalingFactor_WG * numComputation_Forward + numComputation_BP) / (numComputation_Forward + numComputation_BP);
 	}
 
 	cout << "WG: " << scalingFactor_WG << endl;
