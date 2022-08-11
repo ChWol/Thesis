4b97af5a18d2d3b7d3d79be71e5d9584dd0f9c6e
chWol <ch.wolters@tum.de>
2022-08-11 12:33:44 -0400

    setting up inference

--- Training_pytorch/inference.py
+++ Training_pytorch/inference.py
@@ -15,23 +15,23 @@ from datetime import datetime
 from subprocess import call
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR-X Example')
-parser.add_argument('--type', default='cifar10', help='dataset for training')
+parser = argparse.ArgumentParser(description='Evaluation of Biologically-Plausible Learning Rules on Neuromorphic '
+                                             'Hardware Architectures')
+parser.add_argument('--dataset', default='mnist', help='dataset for training')
 parser.add_argument('--batch_size', type=int, default=200, help='input batch size for training')
-parser.add_argument('--epochs', type=int, default=257, help='number of epochs to train')
+parser.add_argument('--epochs', type=int, default=50, help='number of epochs to train')
 parser.add_argument('--grad_scale', type=float, default=1, help='learning rate for wage delta calculation')
 parser.add_argument('--seed', type=int, default=117, help='random seed')
-parser.add_argument('--log_interval', type=int, default=100, help='how many batches to wait before logging training status')
+parser.add_argument('--log_interval', type=int, default=100, help='how many batches to wait before logging training '
+                                                                  'status')
 parser.add_argument('--test_interval', type=int, default=1, help='how many epochs to wait before another test')
 parser.add_argument('--logdir', default='log/default', help='folder to save to the log')
-parser.add_argument('--wl_weight', type=int, default=6, help='weight precision')
-parser.add_argument('--wl_grad', type=int, default=6, help='gradient precision')
 parser.add_argument('--wl_activate', type=int, default=8)
 parser.add_argument('--wl_error', type=int, default=8)
 parser.add_argument('--onoffratio', type=int, default=10)
 parser.add_argument('--cellBit', type=int, default=6, help='cell precision (cellBit==wl_weight==wl_grad)')
 parser.add_argument('--inference', type=int, default=0)
-parser.add_argument('--subArray', type=int, default=32)
+parser.add_argument('--subArray', type=int, default=128)
 parser.add_argument('--ADCprecision', type=int, default=5)
 parser.add_argument('--vari', default=0)
 parser.add_argument('--t', default=0)
@@ -39,29 +39,32 @@ parser.add_argument('--v', default=0)
 parser.add_argument('--detect', default=0)
 parser.add_argument('--target', default=0)
 parser.add_argument('--nonlinearityLTP', type=float, default=1.75, help='nonlinearity in LTP')
-parser.add_argument('--nonlinearityLTD', type=float, default=1.46, help='nonlinearity in LTD (negative if LTP and LTD are asymmetric)')
-parser.add_argument('--max_level', type=int, default=64, help='Maximum number of conductance states during weight update (floor(log2(max_level))=cellBit)')
+parser.add_argument('--nonlinearityLTD', type=float, default=1.46, help='nonlinearity in LTD (negative if LTP and LTD '
+                                                                        'are asymmetric)')
 parser.add_argument('--d2dVari', type=float, default=0, help='device-to-device variation')
 parser.add_argument('--c2cVari', type=float, default=0.003, help='cycle-to-cycle variation')
-parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--network', default='vgg8')
+parser.add_argument('--network', type=int, default=2)
+parser.add_argument('--hidden', type=int, default=1024)
 parser.add_argument('--technode', type=int, default='32')
 parser.add_argument('--memcelltype', type=int, default=3)
-parser.add_argument('--relu', type=int, default=1)
+parser.add_argument('--activation', default='relu')
+parser.add_argument('--rule', default='bp')
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--neurosim', type=int, default=1)
+parser.add_argument('--optimizer', default='adam')
+parser.add_argument('--scheduler', type=int, default=0)
+parser.add_argument('--initial', default='xavier')
+parser.add_argument('--gradient_analysis', type=int, default=0)
 current_time = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
 args = parser.parse_args()
 
 args.max_level = 2 ** args.cellBit
+technode_to_width = {7: 14, 10: 14, 14: 22, 22: 32, 32: 40, 45: 50, 65: 100, 90: 200, 130: 200}
+args.wireWidth = technode_to_width[args.technode]
 
 if args.memcelltype == 1:
     args.cellBit = 1
-
-args.wl_weight = args.cellBit
-args.wl_grad = args.cellBit
-
-technode_to_width = {7: 14, 10: 14, 14: 22, 22: 32, 32: 40, 45: 50, 65: 100, 90: 200, 130: 200}
-args.wireWidth = technode_to_width[args.technode]
 
 args.inference = 0
 args.logdir = os.path.join(os.path.dirname(__file__), args.logdir)
@@ -93,13 +96,13 @@ model_path = (args.logdir + '/best-{}.pth').format(args.epochs - 1)
 assert args.type in ['cifar10', 'cifar100', 'mnist'], args.type
 if args.type == 'cifar10':
     train_loader, test_loader = dataset.get10(batch_size=args.batch_size, num_workers=1)
-    model = model.cifar(args=args, logger=logger, num_classes=10, pretrained=model_path)
+    model = models.cifar(args=args, logger=logger, num_classes=10, pretrained=model_path)
 if args.type == 'cifar100':
     train_loader, test_loader = dataset.get100(batch_size=args.batch_size, num_workers=1)
-    model = model.cifar(args=args, logger=logger, num_classes=100, pretrained=model_path)
+    model = models.cifar(args=args, logger=logger, num_classes=100, pretrained=model_path)
 if args.type == 'mnist':
     train_loader, test_loader = dataset.get_mnist(batch_size=args.batch_size, num_workers=1)
-    model = model.mnist(args=args, logger=logger, pretrained=model_path)
+    model = models.mnist(args=args, logger=logger, pretrained=model_path)
 print(args.cuda)
 if args.cuda:
     model.cuda()
