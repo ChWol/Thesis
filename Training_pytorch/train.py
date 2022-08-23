c6d0961ca99fbd627bd8fb0c484d09947293007e
chWol <ch.wolters@tum.de>
2022-08-23 12:16:24 -0400

    Test on freezing

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -143,8 +143,8 @@ try:
     if args.rule == 'bp':
         for param in model.parameters():
             param.requires_grad = False
-        # for param in model.features[-1].parameters():
-        #     param.requires_grad = True
+        for param in model.features[-1].parameters():
+            param.requires_grad = True
 
     for epoch in range(args.epochs):
         split_time = time.time()
